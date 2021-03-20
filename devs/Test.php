5b128d35db91bff0caa6ba2178450ceb0c606108
ohabille <ohabille@aposte.net>
2021-03-20 13:05:20 +0100

    Test 20/03/2021

--- devs/Test.php
+++ devs/Test.php
@@ -5,58 +5,40 @@ namespace User;
 class Test
 {
     private $_dbName;
+    private $_mainNode;
+    private $_node;
     private $_db;
-    private $_select = [];
+    private $_select;
 
     public function __construct(string $dbName)
     {
         $this->_dbName = $dbName;
+
+        $this->_mainNode = parseConf('db/'.$this->_dbName.'/years');
+
         $this->setNode();
+
+        $this->_select = new \stdClass();
     }
 
-    private function setNode(string $node = 'years') : void
+    private function setNode() : void
     {
-        $this->_db = parseConf('db/'.$this->_dbName.'/'.$node);
+        $this->_node = current($this->_mainNode)->{'mounths'};
     }
 
     public function getTotal() : int
     {
         $nbr = 0;
 
-        foreach ($this->_db as $val) $nbr += $val->{'nbr'};
+        foreach ($this->_mainNode as $val) $nbr += $val->{'nbr'};
 
         return $nbr;
     }
 
     public function getSomeEntries(int $limit, int $step = 1) : void
     {
-        $this->selectYears($limit, $step);
-    }
-
-    private function selectYears(int $limit, int $step) : void
-    {
         $start = self::getStart($limit, $step);
-        $end = self::getEnd($start, $limit);
-        $nbr = 0;
-
-        if (current($this->_db)->{'nbr'} < $start) {
-            dump(key($this->_db));
-            if (false !== next($this->_db)) {
-                $this->selectYears($limit, $step);
-            }
-        }
-
-        if ($nbr <= $limit && self::isCurrent($this->_db)) {
-            $this->_select[] = key($this->_db);
-
-            $limit -= current($this->_db)->{'nbr'};
-
-            echo 'write';
-            if (false !== next($this->_db)) {
-                dump(key($this->_db));
-                $this->selectYears($limit, $step);
-            }
-        }
+        $this->selectYears($start, self::getEnd($start, $limit));
     }
 
     private static function getStart(int $limit, int $step) : int
@@ -69,12 +51,52 @@ class Test
         return $start + $limit;
     }
 
-    private static function isCurrent(\stdClass $objet) : bool
+    private function selectYears(int $start, int $end) : void
     {
-        return false === current($objet) ? false : true;
+        $nbr = count(get_object_vars($this->_select));
+        $limit = $end - $start;
+
+        $nbrEntries = current($this->_node)->{'nbr'};
+
+        if ($nbrEntries < $start) {
+            // echo '<br />'.$start.' to '.$end;
+            $start -= $nbrEntries;
+            $end -= $nbrEntries;
+        }
+        else {
+            // echo '<br />'.$start.' to '.$end.' : '
+            // .key($this->_mainNode).'-'.key($this->_node);
+
+            $db = parseConf(
+                'db/'.$this->_dbName.'/'
+                .key($this->_mainNode).'-'
+                .key($this->_node)
+            );
+
+            $first = $start - 1;
+            $nbrEntries = count(get_object_vars($db));
+            $length = $nbrEntries < $end ?
+                $end - ($end - $nbrEntries) - $first: $end;
+            dump(array_slice(
+                get_object_vars($db), $start - 1, $length, true
+            ));
+        }
+
+        $this->setNext();
+
+        if (false !== current($this->_mainNode))
+            $this->selectYears($start, $end);
     }
 
-    public function getSelect() : array
+    private function setNext() : void
+    {
+        if (false === next($this->_node)) {
+            if (false !== next($this->_mainNode))
+                $this->setNode();
+        }
+    }
+
+    public function getSelect() : \stdClass
     {
         return $this->_select;
     }
