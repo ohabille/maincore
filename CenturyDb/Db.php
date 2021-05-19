31d2b0ee40b944fbf683e2dd2db7e5c3c46512b4
ohabille <ohabille@aposte.net>
2021-05-19 19:02:01 +0200

    WIP CenturyDb 19/05/2021

--- CenturyDb/Db.php
+++ CenturyDb/Db.php
@@ -8,24 +8,90 @@ class Db
      * @var string
      */
     private static $centuryName = 'century-';
+    /**
+     * @var string
+     */
+    private $_dbName;
+    /**
+     * @var int
+     */
     private $_century;
 
-    public function __construct(string $dirName)
+    public function __construct(string $dbName)
     {
-        if ($handle = opendir(__DIR__.'/Db/'.$dirName)) {
-            $pattern = '#'.self::$centuryName.'([1-9A])+.json#';
+        $this->_dbName = __DIR__.'/Db/'.$dbName;
 
+        $this->_century = $this->countCenturies();
+    }
+
+    /**
+     * Compte le nombre de century
+     * @return int : Le nombre de century
+     */
+    private function countCenturies() : int
+    {
+        $i = 0;
+
+        if ($handle = opendir($this->_dbName)) {
             while (false !== ($entry = readdir($handle))) {
-                if (0 == preg_match($pattern, $entry, $out)) continue;
+                if (!self::isCentury($entry))  continue;
 
-                $this->_century = self::$centuryName .= $out[1];
-
-                break;
+                $i++;
             }
 
             closedir($handle);
         }
 
-        echo $this->_century;
+        return $i;
+    }
+
+    private static function isCentury(string $needle) : bool
+    {
+        $patt = "#century-([1-9a]*).json#";
+
+        if (1 === preg_match($patt, $needle)) return true;
+
+        return false;
+    }
+
+    /**
+     * Lit le contenu d'une century
+     * @return array : les données de la century
+     */
+    public function readCentury() : array
+    {
+        return parseConf(
+            $this->_dbName.'/'.self::$centuryName.$this->_century
+        );
+    }
+
+    /**
+     * Compte le nombre d'entrée d'une century
+     * @return int [description]
+     */
+    private function countCentury() : int
+    {
+        return count($this->readCentury());
+    }
+
+    /**
+     * Compte le nombre total du nombre d'entrées
+     * de la base de données
+     * @return int [description]
+     */
+    public function getTotal() : int
+    {
+        return ($this->countCenturies() * 100) - 100 + $this->countCentury();
+    }
+
+    /**
+     * [setNextCentury description]
+     */
+    public function setNextCentury() : void
+    {
+        /*
+        TO DO
+        hexdec & dechex ...
+        */
     }
 }
--- test.php
+++ test.php
@@ -10,4 +10,12 @@ require_once ROOTDIRS.'core/init.php';
 
 $Db = new Db('articles');
 
+$Db->setNextCentury();
+dump($Db->getTotal());
+
+$hexa = dechex("20");
+dump($hexa);
+$dec = hexdec("1aa");
+dump($dec);
+
 ?>
